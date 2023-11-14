7f42a3cb3507b9eea1fa13a0f04bd07b36c912cd
BRight <141350864+brightsablah@users.noreply.github.com>
2023-11-14 12:49:03 +0000

    Update piped_commands.c non-inter leading spaces

--- wip/piped_commands.c
+++ wip/piped_commands.c
@@ -4,31 +4,23 @@
 void handlePipedCommands(const char *command) {
     char *extracted_command = NULL, *cleaned_command;
     size_t command_len = 0;
-    int command_start = -1;
+    int command_start = 0;  // Start from the beginning of the command
     int command_end = -1, i;
 
+    // Find the end of the command
     for (i = 0; command[i] != '\0'; i++) {
         if (command[i] == '"') {
-            if (command_start == -1) {
-                command_start = i + 1;
-            } else {
-                command_end = i;
-                break;
-            }
+            command_end = i;
+            break;
         }
     }
 
-    if (command_start != -1 && command_end != -1) {
+    if (command_end != -1) {
         command_len = command_end - command_start;
         extracted_command = malloc(command_len + 1);
         if (extracted_command != NULL) {
             strncpy(extracted_command, &command[command_start], command_len);
             extracted_command[command_len] = '\0';
-
-            // Remove leading spaces before processing the command
-            while (*extracted_command == ' ' || *extracted_command == '\t') {
-                extracted_command++;
-            }
 
             cleaned_command = remove_extra_spaces(extracted_command);
             if (cleaned_command != NULL) {
