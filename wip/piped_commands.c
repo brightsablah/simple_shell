6805f68874b6900b9d87e3915a79b453a5dc2da3
BRight <141350864+brightsablah@users.noreply.github.com>
2023-11-14 06:32:28 +0000

    Update piped_commands.c spaces

--- wip/piped_commands.c
+++ wip/piped_commands.c
@@ -1,11 +1,27 @@
 #include "shell.h"
 
+#define MAX_ARGS 100
+
 void handlePipedCommands(const char *command) {
+    // Remove leading and trailing spaces
     char *cleaned_command = remove_extra_spaces(command);
-    if (cleaned_command != NULL) {
-        if (is_executable(cleaned_command)) {
-            execute_command(cleaned_command);
-        }
-        free(cleaned_command);
+
+    char *tokens[MAX_ARGS];
+    int token_count = 0;
+
+    // Tokenize the cleaned command based on spaces
+    char *token = strtok(cleaned_command, " ");
+    while (token != NULL && token_count < MAX_ARGS) {
+        tokens[token_count++] = token;
+        token = strtok(NULL, " ");
     }
+
+    // Execute each token as a separate command
+    for (int i = 0; i < token_count; ++i) {
+        if (is_executable(tokens[i])) {
+            execute_command(tokens[i]);
+        }
+    }
+
+    free(cleaned_command);
 }
