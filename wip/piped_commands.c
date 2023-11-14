fbb70e41f6648db46ede728d38cda69b61395b60
BRight <141350864+brightsablah@users.noreply.github.com>
2023-11-14 13:14:35 +0000

    Update piped_commands.c string to token method

--- wip/piped_commands.c
+++ wip/piped_commands.c
@@ -3,45 +3,40 @@
 void handlePipedCommands(const char *command) {
     char *extracted_command = NULL, *cleaned_command;
     size_t command_len = 0;
-    int command_start = 0;
-    int command_end = -1, i;
 
-    // Find the first non-space character
-    while (command[command_start] == ' ') {
-        command_start++;
-    }
-
-    // Find the end of the command (space or end of string)
-    for (i = command_start; command[i] != '\0' && command[i] != ' '; i++) {
-        if (command[i] == '/') {
-            command_start = i;
-            break;
-        }
-    }
-
-    command_end = i;
-
-    // Handle the case where there are leading spaces but no command
-    if (command_start >= command_end) {
+    // Copy the command to a separate buffer for tokenization
+    char *command_copy = strdup(command);
+    if (command_copy == NULL) {
+        // Handle memory allocation failure
         return;
     }
 
-    command_len = command_end - command_start;
-    extracted_command = malloc(command_len + 1);
-    if (extracted_command != NULL) {
-        strncpy(extracted_command, &command[command_start], command_len);
-        extracted_command[command_len] = '\0';
+    // Tokenize the command using strtok
+    char *token = strtok(command_copy, " ");
+    if (token == NULL) {
+        // No command found, free the copy and return
+        free(command_copy);
+        return;
+    }
 
-        cleaned_command = remove_extra_spaces(extracted_command);
-        if (cleaned_command != NULL) {
-            if (is_executable(cleaned_command)) {
-                execute_command(cleaned_command);
-            }
-            free(cleaned_command);
+    // Extract the first token as the command
+    extracted_command = strdup(token);
+    if (extracted_command == NULL) {
+        // Handle memory allocation failure
+        free(command_copy);
+        return;
+    }
+
+    // Clean the extracted command
+    cleaned_command = remove_extra_spaces(extracted_command);
+    if (cleaned_command != NULL) {
+        if (is_executable(cleaned_command)) {
+            execute_command(cleaned_command);
         }
+        free(cleaned_command);
     }
 
-    if (extracted_command != NULL) {
-        free(extracted_command);
-    }
+    // Free allocated memory
+    free(command_copy);
+    free(extracted_command);
 }
