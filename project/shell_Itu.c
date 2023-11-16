d5c56f6446d1696dd07a8b70103ebb88cf02f49a
brikhotso <brenda.rikhotso@gmail.com>
2023-11-15 17:41:29 -0800

    memory management

---
+++ project/shell_Itu.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+void execute_script(FILE *script) {
+    nonInteractiveMode(script);
+}
+/**
+ * print_prompt - Print shell prompt
+ *
+ * Return: void
+ */
+void print_prompt(void)
+{
+	char prompt[] = "$ ";
+
+	write(STDOUT_FILENO, prompt, sizeof(prompt));
+}
+
+
+/**
+ * main - Entry point to the shell
+ * @argc: number of arguments
+ * @argv: The argument values
+ *
+ * Return: 0 on successful exit, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+    char *command_string = NULL;
+    size_t com_len = 0;
+    ssize_t readline;
+    FILE *script;
+
+    if (argc > 1)
+    {
+        script = fopen(argv[1], "r");
+        if (script != NULL)
+        {
+            nonInteractiveMode(script);
+            fclose(script);
+        }
+        else
+        {
+            print_error(argv[0], "failed to open script file");
+            return 1;
+        }
+    }
+    else
+    {
+        while (1)
+        {
+		if (isatty(STDIN_FILENO)) {
+                print_prompt();
+            }
+                      readline = getline(&command_string, &com_len, stdin);
+            if (readline == -1)
+            {
+                exit_shell(NULL);
+                break;
+            }
+            if (readline > 1)
+            {
+                if (command_string[readline - 1] == '\n')
+                    command_string[readline - 1] = '\0';
+
+                execute_command(command_string);
+            }
+        }
+        free(command_string);
+    }
+
+    return 0;
+}
