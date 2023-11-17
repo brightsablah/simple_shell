b42d5f84ca3ad5cb98fde176eb7da19de7b66edf
Bright <bright.sablah@outlook.com>
2023-11-17 22:16:23 +0000

    created and added _strncat function in an individual file

---
+++ 2deadln/_strncat.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _strncat - concentrates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes used
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, len = 0;
+
+	while (dest[i++])
+		len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[len++] = src[i];
+
+	return (dest);
+}
+
