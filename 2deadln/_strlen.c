31e188d0596699c2e81ee2b6eac5e00711a2ded0
Bright <bright.sablah@outlook.com>
2023-11-17 22:16:23 +0000

    created and added _strlen function in an individual file

---
+++ 2deadln/_strlen.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns leghth of a string
+ * @s: string
+ *
+ * Return: lenght of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	return (i);
+}
