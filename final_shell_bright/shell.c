929d1e37e2355c3b01b1f6cf156bda22f7d51ada
BRight <141350864+brightsablah@users.noreply.github.com>
2023-11-18 02:13:32 +0000

    Update shell.c exit_shell(NULL) to exit(0)

--- final_shell_bright/shell.c
+++ final_shell_bright/shell.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
             if (readline == -1)
             {
 		    free(command_string);  /* Free memory if getline fails */
-                exit_shell(NULL);
+                exit(0);
                 break;
             }
 
