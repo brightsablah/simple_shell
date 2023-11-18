fb91308c9058ac1d9772e50bca59b4fc84cd09fb
BRight <141350864+brightsablah@users.noreply.github.com>
2023-11-18 01:51:31 +0000

    Update _getenv.c revert function

--- final_shell_bright/_getenv.c
+++ final_shell_bright/_getenv.c
@@ -1,28 +1,49 @@
 #include "shell.h"
 
 /**
- * _getenv - Get the value of an environment variable.
- * @name: Name of the environment variable.
+ * _getenv - Retrieve value of environment variable
+ * @name: Name of environment variable to retrieve
  *
- * Return: Pointer to the value of the environment variable,
- * or NULL if the variable is not found.
+ * Return: Value of the environment variable if found, NULL if not found
  */
-char *_getenv(const char *name) {
-    char **env;
-    
-    if (name == NULL || *name == '\0') {
-        return NULL; /* Invalid input */
-    }
+char *_getenv(const char *name)
+{
+	char **env = environ;
+	char *env_var;
+	const char *name_ptr;
+	size_t value_length;
+	char *value;
 
-    /* Iterate through the environment variables */
-    for (env = environ; *env != NULL; env++) {
-        /* Check if the current environment variable starts with the specified name */
-        if (strncmp(*env, name, strlen(name)) == 0 && (*env)[strlen(name)] == '=') {
-            /* Return the value part of the environment variable */
-            return *env + strlen(name) + 1;
-        }
-    }
+	env_var = *env;
 
-    return NULL; /* Variable not found */
+	while (*env)
+	{
+		name_ptr = name;
+		while (*name_ptr && *env_var == *name_ptr)
+		{
+			env_var++;
+			name_ptr++;
+		}
+		if (*name_ptr == '\0' && *env_var == '=')
+		{
+			env_var++; /* Move to the value part after '=' */
+
+			value_length = _strlen(env_var);
+			value = malloc(value_length + 1);
+
+			if (value != NULL)
+			{
+				_strcpy(value, env_var);
+				return (value);
+			}
+			else
+			{
+			       
+				return (NULL);
+			}
+		}
+		env++;
+		env_var = *env;
+	}
+	return (NULL);
 }
-
